f1ad512b74ad3a5e598057d3e466ac4887335756
victor_marquez <victorsheepman@gmail.com>
2024-01-10 11:26:18 -0400

    Comienzo del desarrollo

--- MarvelApp/ViewController.swift
+++ MarvelApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("hola mmoto")
+        print("hola initia")
     }
 
 
