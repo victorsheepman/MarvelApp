47830265cf24807dc777a737d6cc3ae16c8a1fe9
victor_marquez <victorsheepman@gmail.com>
2024-03-14 09:55:34 -0400

    created two functions

--- MarvelApp/Detail/Controller/DetailViewController.swift
+++ MarvelApp/Detail/Controller/DetailViewController.swift
@@ -9,16 +9,44 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     var heroId:Int?
     private let dataManager = ExternalDataManager()
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataManager.heroDetailDelegate = self
         if let id = heroId {
             dataManager.getCharacterById(id: id)
         }
+        configureImageView()
     }
+    
+    private func configureImageView(){
+        
+        imageView.image = UIImage(named: "bg4")
+        imageView.contentMode = .scaleToFill
+        imageView.layer.insertSublayer(createGradientImage(image: imageView), at: 0)
+        imageView.backgroundColor = .clear
+    }
+    
+    private func createGradientImage(image:UIImageView) -> CAGradientLayer{
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = image.bounds
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        
+        return gradientLayer
+    }
+    
+    
+    
+    
+    
 }
 
 extension DetailViewController:GetHeroDetailProtocol {
