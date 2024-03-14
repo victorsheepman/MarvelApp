4899592d94a1c0504ae5df0872c07745f42fdb1d
victor_marquez <victorsheepman@gmail.com>
2024-03-14 10:18:07 -0400

    add activity

--- MarvelApp/Detail/Controller/DetailViewController.swift
+++ MarvelApp/Detail/Controller/DetailViewController.swift
@@ -11,6 +11,7 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var activity: UIActivityIndicatorView!
     
     var heroId:Int?
     private let dataManager = ExternalDataManager()
@@ -21,17 +22,29 @@ class DetailViewController: UIViewController {
         if let id = heroId {
             dataManager.getCharacterById(id: id)
         }
-        configureImageView()
+        
+        startActivity()
+        nameLabel.text = ""
     }
     
+    
+    
+    private func startActivity(){
+        activity.startAnimating()
+    }
+    
+    private func stopActivity(){
+        activity.stopAnimating()
+        activity.isHidden = true
+        configureImageView()
+    }
     private func configureImageView(){
         
-        imageView.image = UIImage(named: "bg4")
+        //imageView.image = UIImage(named: "bg4")
         imageView.contentMode = .scaleToFill
         imageView.layer.insertSublayer(createGradientImage(image: imageView), at: 0)
         imageView.backgroundColor = .clear
     }
-    
     private func createGradientImage(image:UIImageView) -> CAGradientLayer{
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = image.bounds
@@ -52,5 +65,10 @@ class DetailViewController: UIViewController {
 extension DetailViewController:GetHeroDetailProtocol {
     func getHeroDetail(hero: [Result]) {
         print(hero)
+        
+        DispatchQueue.main.async {
+            self.nameLabel.text = hero[0].name
+            self.stopActivity()
+        }
     }
 }
